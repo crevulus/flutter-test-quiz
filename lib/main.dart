c538778316b5b7aa8e7f3283af8da7b060c3c89c
crevulus <crevulus@gmail.com>
2020-06-02 19:17:50 +0200

    functional buttons, separated question widget, began styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> { // adding _ turns public class to private class
+  var _questionIndex = 0;
+
+  void _givenAnswer() {
+    setState(() { // calls build on widget where you call setState (in this case MyApp)
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s love got to do with it?',
+      'Where is the love?',
+      'How deep is your love?'
+    ];
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text('My First App')),
       body: Column(children: [
-        Text('Q1'),
-        RaisedButton(child: Text('A1'), onPressed: null),
-        RaisedButton(child: Text('A2'), onPressed: null),
-        RaisedButton(child: Text('A3'), onPressed: null)
+        Question(questions[_questionIndex]),
+        RaisedButton(child: Text('A1'), onPressed: _givenAnswer), // pass name of function as onPressed requires a fn, not the return value of a fn
+        RaisedButton(child: Text('A2'), onPressed: _givenAnswer),
+        RaisedButton(child: Text('A3'), onPressed: _givenAnswer)
       ],))
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText; // final = value doesn't change after constructor has been initialised
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity, // .infinity equivalent to 100% on CSS
+      margin: EdgeInsets.all(10),
+      child: Text( // Text widgets only take as much space as they need by default, so need to wrap
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
