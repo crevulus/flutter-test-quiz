334ae093aff7ac0a6fba0ce6207eaaef8f414fbf
crevulus <crevulus@gmail.com>
2020-06-04 16:21:16 +0200

    can't figure out scrollable listview

--- lib/listicle.dart
+++ lib/listicle.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
 
-class Listicle extends StatelessWidget {
+class Listicle extends StatefulWidget {
+  @override
+  ListicleState createState() => ListicleState();
+}
+
+class ListicleState extends State<Listicle> {
+  final List<String> entries = <String>['A', 'B', 'C', 'D', 'E', 'F'];
+  final List<int> colorCodes = <int>[600, 500, 100, 600, 500, 100];
+
+  ScrollController _controller = new ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView(
+      child: ListView.builder(
+//      physics: const AlwaysScrollableScrollPhysics(),
+//      controller: _controller,
         shrinkWrap: true,
         padding: const EdgeInsets.all(8),
-        children: <Widget>[
-          Container(
-            height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('Entry A')),
-          ),
-          Container(
-            height: 50,
-            color: Colors.amber[500],
-            child: const Center(child: Text('Entry B')),
-          ),
-          Container(
-            height: 50,
-            color: Colors.amber[100],
-            child: const Center(child: Text('Entry C')),
-          ),
-        ],
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 100,
+            color: Colors.amber[colorCodes[index]],
+            child: Center(child: Text('Entry ${entries[index]}')),
+          );
+        }
       )
     );
   }
 }
+
