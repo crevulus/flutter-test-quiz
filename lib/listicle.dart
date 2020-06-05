99a33a76481a8ef4bdfbdf834f2011d5c01f98e9
crevulus <crevulus@gmail.com>
2020-06-05 10:23:56 +0200

    figured out scrolling list

--- lib/listicle.dart
+++ lib/listicle.dart
@@ -9,26 +9,27 @@ class ListicleState extends State<Listicle> {
   final List<String> entries = <String>['A', 'B', 'C', 'D', 'E', 'F'];
   final List<int> colorCodes = <int>[600, 500, 100, 600, 500, 100];
 
-  ScrollController _controller = new ScrollController();
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-//      physics: const AlwaysScrollableScrollPhysics(),
-//      controller: _controller,
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 100,
-            color: Colors.amber[colorCodes[index]],
-            child: Center(child: Text('Entry ${entries[index]}')),
-          );
-        }
-      )
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 400,
+          child: ListView.builder(
+              primary: false,
+              physics: const AlwaysScrollableScrollPhysics(),
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8),
+              itemCount: entries.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 100,
+                  color: Colors.green[colorCodes[index]],
+                  child: Center(child: Text('Entry ${entries[index]}')),
+                );
+              }),
+        ),
+      ],
     );
   }
 }
-
